7a391d75aeff823b2359ff9fd383c4e4703767d6
Andreas Søvik <arsovik@gmail.com>
2019-09-20 09:03:00 +0200

    Fix CSS and naming of typecheckers

--- src/types-smart.ts
+++ src/types-smart.ts
@@ -2,12 +2,12 @@ import typesRuntime from './types-ti';
 import { createCheckers } from 'ts-interface-checker';
 import { Breeds, Owners } from './types';
 
-const { Owners, Breeds } = createCheckers(typesRuntime);
+const { Owners: OwnersChecker, Breeds: BreedsChecker } = createCheckers(typesRuntime);
 
 export const isOwner = function(data: any): data is Owners {
-  return Owners.validate(data) === null;
+  return OwnersChecker.validate(data) === null;
 }
 
 export const isBreeds = function(data: any): data is Breeds {
-  return Breeds.validate(data) === null;
+  return BreedsChecker.validate(data) === null;
 }
